69b82ac6f4f0788c3efad33eab9533f93d49f915
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-10-02 01:54:37 +0800

    kill yourself

--- config.py
+++ config.py
@@ -1,21 +1,18 @@
-TOKEN = "MTEzNDQxOTU2MzQ4OTAyMTk1Mw.GTjtdG.6qV5Sd83gh3FLANVVEbTcPFOCR1Ib5HY27nihU"
+TOKEN = ""
 SAVE_DIR = "saves"
 
-# inference config
-MODEL_DIR = "/Users/JLOLC/programming/onGAU/onGAU/models"
-MODEL_PATHS = {
-    # "absolutereality": "models/AbsoluteReality",
-    # "sketchydreams": "models/dreamsArchive_sketchydreams",
-    # "aom3": "models/abyssorangemix3AOM3_aom3a3",
-    # "cetus": "models/cetusMix_cetusVersion3",
-    # "hassaku": "models/hassakuHentaiModel_v12",
-    # "flat2d": "models/safetensors/flat2DAnimerge_v30.safetensors",
-    # "meinapastel": "models/safetensors/meinapastel_v6Pastel.safetensors",
-    "aurora": "models/aurora-mix/v2",
-}
+# name and path to models
+# example:
+# {
+#     "aurora": "models/aurora-v2",
+#     "hassaku": "models/hassaku.safetensors"
+# }
+MODEL_PATHS = {}
 
-DEVICE = "mps"
-EMBEDDINGS = ["models/embeds/easynegative.pt", "models/embeds/deepnegative.pt"]
+DEVICE = "cuda"
+
+# list of paths to embedding and lora models
+EMBEDDINGS = []
 LORAS = []
 
 PROGRESS_EMBED_COLOR = "blue"
